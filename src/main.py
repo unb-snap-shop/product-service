7d03a62590c930ffa18773bbc8ce48d478e47843
Eric599 <cuenat.eric@gmail.com>
2024-03-06 02:07:39 -0400

    Add Coors

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from api.routes import catalogue_blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(catalogue_blueprint, url_prefix='/api')
 
